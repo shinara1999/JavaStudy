2161a4977c2821231ed96ccd1aaa800a94dfefe3
SIST <SIST@DESKTOP-H16B7VH>
2023-09-12 17:10:10 +0900

    1111

---
+++ Chapter02_데이터형/src/자바변수연습.java
@@ -0,0 +1,23 @@
+/*
+ * 		국어, 영어, 수학 점수를 받아서 총점과 평균을 구하는 프로그램을 작성
+ * 		=> 학점을 출력해라 char
+ */
+public class 자바변수연습 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kor=90;
+		int eng=85;
+		int mat=70;
+		
+		int total=kor+eng+mat;
+		double ave=(kor+eng+mat)/3;
+		// char 학점=' '; // 붙이지 말고 space나 값 하나를 줘야한다
+		
+		// int kor=0, eng=0, mat=0, total=kor+eng+mat; => 같은 데이터형은 모아서 쓸 수 있다.
+		
+		System.out.println("total:"+total);
+		System.out.println("ave:"+ave);
+	}
+
+}
---
+++ Chapter02_데이터형/src/자바변수연습2.java
@@ -0,0 +1,17 @@
+/*
+ * 		정수 3개를 입력받아서 최대값과 최소값을 구해라.
+ */
+public class 자바변수연습2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 초기값을 주는 방법
+		// int a,b,c;
+		// a=b=c=0;
+		// int max=0, min=0;
+		
+		// System.out.println(a);
+	}
+
+}
