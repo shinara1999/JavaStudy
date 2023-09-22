26ce55c2ec21d73a46ab84797cb188327bfcbf02
SIST <SIST@DESKTOP-H16B7VH>
2023-09-22 17:44:46 +0900

    1111

---
+++ Chapter03_제어문연습문제(GPT)/src/문제1.java
@@ -0,0 +1,23 @@
+// 1. 사용자로부터 정수 n을 입력받아, n개의 정수를 입력받은 후 이들의 평균을 계산하는 프로그램을 작성하세요. (for 또는 while 문 사용)
+
+import java.util.Scanner;
+public class 문제1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+//		int sum=0;
+//		double avg=0;
+//		
+//		for(int i=1;i<=n;i++)
+//		{
+//				System.out.println("정수 입력:");
+//				int n=scan.nextInt();
+//		}
+		
+		
+
+	}
+
+}
