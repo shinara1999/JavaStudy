8aed73cc70a6c08214afec953c97ef188f2261fe
SIST <SIST@DESKTOP-H16B7VH>
2023-10-12 16:03:15 +0900

    1111

---
+++ Chapter05_접근지정어/src/com/sist/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.sist.main; // 패키지
+import com.sist.util.User; // 다른 패키지의 클래스를 가져올 때는 import로 가져온다.
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainClass2 m=new MainClass2(); // 같은 패키지 안에서는 import 없이 사용 가능
+
+	}
+
+}
